0820ff4313a4cc8531b79219756551cd80435069
Vaishnav Murali <Vaishnav Murali>
2017-03-08 11:22:26 +0530

    perceptron.c: Added Lots of shit, Simplified shit
    Made code clean and added few functions

--- perceptron.c
+++ perceptron.c
@@ -2,21 +2,65 @@
 #include<math.h>
 #include <stdlib.h>
 #include <time.h>
-float ***weights[];
-float **data;
+/*
+MULTILAYER PERCEPTRON!
+
+FORMULA TO BE USED IN ERROR BETWEEN HIDDEN NODE AND OUTPUT LAYER:
+E`(wrt node k in hidden layer)=-(desired_output_k-current_output_k)*f`(sum_of_weights_with_hiddennode_values)
+
+FORMULA TO BE USED IN ERROR BETWEEN HIDDEN NODE AND OUTPUT LAYER:
+E`(wrt node j in input layer)=sum(-error_in_hidden_layer*weight_of_kj*f`(sum_of_weights_with_hidden_node_values)*input_node_i)
+
+3D weight matrix
+1st Dimension - Layer
+2nd & 3rd Dimension - locating the weight
+*/
+FILE *train,*test;
+float **prevhiddenweights;
+float **previnputweights;
+float ***weights;
+float *data;
+float *hiddendata;
 float LR;
 int Number_neurons[3]={13,-1,3};
 int currtime=0;
+float **errorhidden;
+float **errorinput;
 srand((unsigned int)time(NULL));
-/*struct node
+void init(int n)
 {
-	float data[13];
-	int dclass;
-};*/
-/*float sigmoidfun1(float x)
-{
-	return tanh(x);
-}*/
+		Number_neurons[1]=n;
+		weights=new float**[2];
+		int i=0,j=0,k=0;
+		for(;i<2;i++)
+		{
+			if(i)
+			{
+					weights[i]=new float*[Number_neurons[1]];
+					for(j=0;j<Number_neuron[1];j++)
+					{
+							weights[i][j]=new float[Number_neurons[0]];
+							for(k=0;k<Number_neurons[0];k++)
+							{
+								weights[i][j][k]=randomweights();
+							}
+					}
+			}
+			else
+			{
+				weights[i]=new float*[Number_neuron[2]];
+				for(j=0;j<Number_neuron[2];j++)
+				{
+						weights[i][j]=new float[Number_neurons[1]];
+						for(k=0;k<Number_neurons[1];k++)
+						{
+							weights[i][j][k]=randomweights();
+						}
+				}
+			}
+			
+		}
+}
 float sigmoidfun2(float x)
 {
 	return pow(1+exp(-1*x),-1);
@@ -25,26 +69,11 @@ float derivativefun2(float x)
 {
 	return (float)sigmoidfun2(x)*(1-sigmoidfun2(x));
 }
-void randomweights(int index,int layer)
+float randomweights()
 {
-	/*
-		Ideal initial range=√(6/(num_of_neuron_layer[i]+num_of_neuron_layer[i-1]))
-	*/
-	int i=0;
+	return ((float)rand()/(float)(RAND_MAX))*(sqrt(6/(Number_neurons[layer]+Number_neurons[layer-1])));
+}
+void backpropogation()
+{
 	
-	for(;i<Number_neurons[layer];i++)
-	{
-		weight[currtime][layer][index][i]=((float)rand()/(float)(RAND_MAX))*(sqrt(6/(Number_neurons[layer]+Number_neurons[layer-1])));
-	}
-}
-float summation(int index,int layer)
-{
-	float sum=0.0;
-	for(i=0;i<Number_neurons[layer];i++)
-		sum+=weight[currtime][layer][index][i]
-	return sum;	
-}
-void update_weight(int layer,int index)
-{
-	w
 }
